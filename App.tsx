d561e7dc3f48a6b200c6800caab6adf4b9367bbb
camjla <camila.tronco@hotmail.com>
2023-07-11 14:01:48 -0300

    Inicializando

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import  Main from './src'
+
+
+export default function App() {
+  return (
+    <Main >
+          
+    </Main>
+  );
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+const Main: React.FC = () => {
+  return (
+    <View >
+           <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default Main;
